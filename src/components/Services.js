d5b60045b2508350f0ac361008344128163d298f
Chris Dietrich <chrisdietrich3666@gmail.com>
2021-08-18 08:07:17 -0400

    improving images on services page

--- src/components/Services.js
+++ src/components/Services.js
@@ -39,54 +39,44 @@ export default class Services extends React.Component {
                 <div className="image"></div>
               </div>
               <div className="services-description" id="snow-removal">
-                {/* <img src="/images/lawn-care.png" alt=""/> */}
                 <div className="image"></div>
                 <div className="services-image-description">
                   <h3>Snow Removal</h3>
                   <p>If you are stuck in your property and you have hard access outside, grab the phone and call us. You don’t want to risk being stuck for hours or going out at the expense of your safety. We’ll help you get rid of snow and keep your sidewalk clear.</p>
                 </div>
               </div>
-            </div>
-            <div className="services-container">
-              <div className="services-description" id="snow-removal">
-                <img src="/images/snow-removal.png" alt=""/>
-                <div className="services-image-description">
-                  <h3>Snow Removal</h3>
-                  <p>If you are stuck in your property and you have hard access outside, grab the phone and call us. You don’t want to risk being stuck for hours or going out at the expense of your safety. We’ll help you get rid of snow and keep your sidewalk clear.</p>
-                </div>
-              </div>
-            </div>
-            <div className="services-container">
               <div className="services-description" id="stump-removal">
                 <div className="services-image-description">
                   <h3>Stump Removal</h3>
                   <p>We provide the best stump removal service in the Baltimore area. We provide a variety of services, including stump grinding and removal. We have the skill and experience necessary to remove all types of tree stumps, offering the highest quality services and customer care.</p>
                 </div>
-                <img src="/images/StumpRemoval.jpg" alt=""/>
+                <div className="image"></div>
               </div>
-            <div className="services-container">
-            <div className="services-description" id="demolition">
-              <img src="/images/trash.jpg" alt=""/>
-              <div className="services-image-description">
-                <h3>Demolition and Hauling</h3>
-                <p>Our trained professionals provide cleanout and trash-removal services for residential and commercial properties, as well as construction and renovation sites.
-                We value prompt, professional and courteous service and take the time to inspect every job and location individually. Many of our reviews contain comments on how our customers appreciated our being on time.</p>
+              <div className="services-description" id="demolition">
+                <div className="image"></div>
+                <div className="services-image-description">
+                  <h3>Demolition & Hauling</h3>
+                  <p>Our trained professionals provide cleanout and trash-removal services for residential and commercial properties, as well as construction and renovation sites. We value prompt, professional and courteous service and take the time to inspect every job and location individually. Many of our reviews contain comments on how our customers appreciated our being on time.</p>
+                </div>
+              </div>
+              <div className="services-description" id="regrades">
+                <div className="services-image-description">
+                  <h3>Regrades</h3>
+                  <p>Most clients we visit can benefit from some form of grading to further get water away from their houses. Negative grading creates hydrostatic pressure on walls by constantly forcing water to flow towards the house. With high-density clay fill, we change the grade around your entire house to create a holistic system that deters water from coming near the vulnerable points around the house and property.</p>
+                </div>
+                <div className="image"></div>
               </div>
             </div>
-            <div className="services-container">
-            <div className="services-description" id="regrades">
-              <div className="services-image-description">
-                <h3>Regrades</h3>
-                <p>Most clients we visit can benefit from some form of grading to further get water away from their houses. Negative grading creates hydrostatic pressure on walls by constantly forcing water to flow towards the house. With high-density clay fill, we change the grade around your entire house to create a holistic system that deters water from coming near the vulnerable points around the house and property.</p>
-              </div>
-              <img src="/images/Regrades.jpg" alt=""/>
-            </div>
+          
+            
+         
+            
 
           </div>
           
-          </div>
-          </div>
-          </div>
+     
+      
+         
           
          
       
