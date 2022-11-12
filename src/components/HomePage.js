76f6a10172540e9987c6a2ccd8c0160ee6e36eb2
Chris <chrisdietrich3666@gmail.com>
2022-11-12 18:01:29 -0500

    feat styled home page

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -37,23 +37,30 @@ export default class HomePage extends React.Component {
 
         <div className="home-page-bottom-container">
           <section className="home-page-bottom-section">
-            <div className="box1">
+            <h1>Dietrich Landcare</h1>
+            <div className="home-page-image">
               <img src={lawnCare} alt="" className="bottom-pic" />
             </div>
-            <div className="box2">
+            <div className="home-page-description">
               <p>
                 Our professionally trained landscape designers are committed to
                 surpassing your expectations and doing everything we can to
-                ensure you are proud of your landscape. <br /> We offer a number
-                of different landscape services that you can choose from, and we
-                take the time to customize it to your specific needs in order
-                for it to reflect you best and ensure it aligns to the outdoor
-                activities you enjoy the most. <br /> We are certified in all
-                aspects of landscaping that offers you efficient and dependable
-                services that suit your budget and needs best.We craft
-                innovative patios, terraces, retaining walls, dry rock creeks,
-                outdoor fireplaces, walkways, energy-efficient lighting and
-                more.
+                ensure you are proud of your landscape.
+              </p>
+              <p>
+                {" "}
+                We offer a number of different landscape services that you can
+                choose from, and we take the time to customize it to your
+                specific needs in order for it to reflect you best and ensure it
+                aligns to the outdoor activities you enjoy the most.{" "}
+              </p>{" "}
+              <p>
+                {" "}
+                We are certified in all aspects of landscaping that offers you
+                efficient and dependable services that suit your budget and
+                needs best.We craft innovative patios, terraces, retaining
+                walls, dry rock creeks, outdoor fireplaces, walkways,
+                energy-efficient lighting and more.
               </p>
             </div>
             <div className="box3"></div>
